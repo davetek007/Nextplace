13cbd62c8d94651aa7229c925c2145c93d2a7dce
David Halfteck <david.halfteck@accessitautomation.com>
2025-05-08 14:40:54 +0100

    housecoin

--- Nextplace.Api/Controllers/NextplaceController.cs
+++ Nextplace.Api/Controllers/NextplaceController.cs
@@ -3,13 +3,16 @@ using Swashbuckle.AspNetCore.Annotations;
 using System.Reflection;
 using Azure.Identity;
 using Azure.Storage.Blobs;
+using Newtonsoft.Json;
+using Nextplace.Api.Db;
+using Nextplace.Api.Helpers;
 
 namespace Nextplace.Api.Controllers;
 
 [Tags("Nextplace APIs")]
 [ApiController]
 [Route("Nextplace")]
-public class NextplaceController(IConfiguration config) : ControllerBase
+public class NextplaceController(AppDbContext context, IConfiguration config) : ControllerBase
 {
   [HttpGet("/Version", Name = "GetVersion")]
   [SwaggerOperation("Get product version")]
@@ -17,6 +20,38 @@ public class NextplaceController(IConfiguration config) : ControllerBase
   {
     var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
     return Ok(new { version });
+  }
+
+  [HttpGet("/HousecoinPrice", Name = "GetHouseCoinData")]
+  [SwaggerOperation("Get the current data of HouseCoin (HOUSE) in USD")]
+  public async Task<ActionResult> GetHouseCoinPrice()
+  {
+    var executionInstanceId = HttpContext.Items["executionInstanceId"]?.ToString()!;
+    await context.SaveLogEntry("GetHouseCoinPrice", "Initiated", "Information", executionInstanceId);
+
+    var apiKey = await new AkvHelper(config).GetSecretAsync("CoinMarketCapApiKey");
+
+    using var httpClient = new HttpClient();
+    httpClient.DefaultRequestHeaders.Add("X-CMC_PRO_API_KEY", apiKey);
+    httpClient.DefaultRequestHeaders.Add("Accepts", "application/json");
+
+    var url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=HOUSE&convert=USD";
+
+    try
+    {
+      var response = await httpClient.GetAsync(url);
+      response.EnsureSuccessStatusCode();
+      var json = await response.Content.ReadAsStringAsync(); 
+
+      await context.SaveLogEntry("GetHouseCoinPrice", "Successfully fetched CoinMarketCap data", "Information", executionInstanceId);
+
+      return Content(json, "application/json");
+    }
+    catch (Exception ex)
+    {
+      await context.SaveLogEntry("GetHouseCoinPrice", $"Error: {ex.Message}", "Error", executionInstanceId);
+      return StatusCode(500, "Failed to retrieve HouseCoin price.");
+    }
   }
 
   [HttpGet("/DownloadData/{id}", Name = "DownloadData")]
